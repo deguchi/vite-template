6bed04e807ced2c8411f85fcbe16b8eec8aad28c
deguchi <deguchik@gmail.com>
2022-04-21 11:00:49 +0900

    update

--- vite.config.ts
+++ vite.config.ts
@@ -5,9 +5,7 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  root: "src",
   build: {
-    outDir: resolve(__dirname, "dist"),
     rollupOptions: {
       output: {
         entryFileNames: `assets/[name].js`,
